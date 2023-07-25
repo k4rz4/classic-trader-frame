1f7defc309fc706557c49e47c277c37b90b472a5
k4rz4 <karzicmarko@gmail.com>
2023-07-25 00:25:01 -0400

    Formating change

--- api/Core/HttpClient.php
+++ api/Core/HttpClient.php
@@ -19,26 +19,26 @@ class HttpClient
     }
 
     public function get(string $url, array $headers = []): Response
-{
-    try {
-        $httpResponse = $this->client->request('GET', $url, [
-            'headers' => $headers
-        ]);
+    {
+        try {
+            $httpResponse = $this->client->request('GET', $url, [
+                'headers' => $headers
+            ]);
 
-        $body = $httpResponse->getBody()->getContents();
-        $statusCode = $httpResponse->getStatusCode();
-        $headers = $httpResponse->getHeaders();
-        // Convert headers to associative array
-        $headersAssoc = [];
-        foreach ($headers as $name => $values) {
-            $headersAssoc[$name] = implode(', ', $values);
+            $body = $httpResponse->getBody()->getContents();
+            $statusCode = $httpResponse->getStatusCode();
+            $headers = $httpResponse->getHeaders();
+            // Convert headers to associative array
+            $headersAssoc = [];
+            foreach ($headers as $name => $values) {
+                $headersAssoc[$name] = implode(', ', $values);
+            }
+
+            return new Response($statusCode, $body, $headersAssoc);
+        } catch (GuzzleException $e) {
+            $this->logger->error('Uncaught exception: ' . $e->getMessage());
+            return new Response(500, $e->getMessage());
         }
-
-        return new Response($statusCode, $body, $headersAssoc);
-    } catch (GuzzleException $e) {
-        // Handle the exception 
-        return new Response(500, $e->getMessage());
     }
-}
 
 }
